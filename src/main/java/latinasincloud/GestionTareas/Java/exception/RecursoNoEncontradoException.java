1fb5274b37fd90a1d7c13e1cea7266c117fcf1d8
Bernarda Rodriguez Cuevas <bernarda.rodriguez.cuevas@gmail.com>
2025-10-11 17:51:52 -0300

    docs: Se agrega packages exception creacion de excepciones personalizadas y un controllerAdvice de manejo de excepciones en toda la aplicacion.

---
+++ src/main/java/latinasincloud/GestionTareas/Java/exception/RecursoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package latinasincloud.GestionTareas.Java.exception;
+
+public class RecursoNoEncontradoException extends RuntimeException {
+    public RecursoNoEncontradoException(String message) {
+        super(message);
+    }
+}
