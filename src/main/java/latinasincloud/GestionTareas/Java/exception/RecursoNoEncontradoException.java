ccd097f70a403c2199c76e83da778740fb42ad39
Bernarda Rodriguez Cuevas <bernarda.rodriguez.cuevas@gmail.com>
2025-10-11 19:08:06 -0300

    docs: modificacion de exception personalizada RecursoNoEncontradoException

--- src/main/java/latinasincloud/GestionTareas/Java/exception/RecursoNoEncontradoException.java
+++ src/main/java/latinasincloud/GestionTareas/Java/exception/RecursoNoEncontradoException.java
@@ -1,5 +1,6 @@
-package latinasincloud.GestionTareas.Java.exception;
+package latinasincloud.GestionTareas.Java.exception; // recordar para exception Crear un nuevo paquete 'exception'
 
+// Extiende RuntimeException para ser una excepción no chequeada
 public class RecursoNoEncontradoException extends RuntimeException {
     public RecursoNoEncontradoException(String message) {
         super(message);
