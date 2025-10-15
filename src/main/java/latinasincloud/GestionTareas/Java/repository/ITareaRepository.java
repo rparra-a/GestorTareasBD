80005fe59f374616c213710aa001c0c4aab85fc9
Bernarda Rodriguez Cuevas <bernarda.rodriguez.cuevas@gmail.com>
2025-10-15 02:52:08 -0300

    docs: Cambios solo en la documentación, comentarios de sintaxis.

--- src/main/java/latinasincloud/GestionTareas/Java/repository/ITareaRepository.java
+++ src/main/java/latinasincloud/GestionTareas/Java/repository/ITareaRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+// esta interfaz hereda todos los métodos CRUD
+
 public interface ITareaRepository extends JpaRepository<Tarea,Integer> {
     List<Tarea> findByEstado_Nombre(String nombre);
 }
